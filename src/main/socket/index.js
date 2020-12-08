ba966bed6f8bc40758b3a323d3e2a5e7e2353bc7
liting <123@qq.com>
2020-12-08 18:17:10 +0800

    代码优化

--- src/main/socket/index.js
+++ src/main/socket/index.js
@@ -13,6 +13,8 @@ const os = require('os');
 class CreateSocket{
     constructor(){
         this.wss = null;
+        // 分享视频头帧
+        this.header = null;
     }
     init(){
         this.createSocket();
@@ -40,34 +42,49 @@ class CreateSocket{
     }
 
     // 进行广播函数
-    broadcast(data) {
+    broadcast(data, ws) {
         this.wss.clients.forEach(client => {
+            // 广播到包括自己的用户
             if(client.readyState == WebSocket.OPEN){
                 client.send(data);
             }
+            // 广播不包括自己的用户
+            // if (client !== ws && client.readyState === WebSocket.OPEN) {
+            //     client.send(data);
+            // }
         })
     }
 
     addEventListen(){
         // 当有客户端连接时触发
         this.wss.on('connection', ws =>{
-            console.log("有客户端连接")
+            this.connectionHandle(ws);
             // 接收客户端发送的消息
-            ws.on('message', data => {
-                console.log('接收数据：', typeof data, data);
-                this.broadcast(data);
-            })
+            ws.on('message', data => this.messageHandle(data, ws))
 
             // 当客户端关闭连接时触发
-            ws.on('close', ev => {
-                console.log('客户端断开连接')
-            })
+            ws.on('close', ev => this.closeHandle(ev));
 
             // 当发送错误时触发
-            ws.on('error', err=>{
-                console.log(err);
-            })
+            ws.on('error', err=> this.errorHandle(err));
         })
+    }
+    // 处理客户端发送的消息
+    messageHandle(data, ws){
+        // 将数据进行广播
+        this.broadcast(data, ws);
+    }
+    // 处理客户端断开连接
+    closeHandle(ev){
+        console.log('客户端断开连接')
+    }
+    // 推送消息出现错误
+    errorHandle(err){
+         console.log(err);
+    }
+    // 连接成功
+    connectionHandle(ws){
+        console.log("有客户端连接")
     }
 }
 
--- src/pages/js/initiate.js
+++ src/pages/js/initiate.js
@@ -3,127 +3,124 @@
  * @Date: 2020-11-26 19:38:27
  * @LastEditTime: 2020-12-08 00:25:11
  * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
+ * @Description: 发起桌面共享页面
  * @FilePath: \electron-vue-template\src\pages\js\update.js
  */
+
 const { desktopCapturer } = require('electron');
+class Initiate {
+    constructor(){
+        this.video = this.getEl('#move');
+        this.client = null;
+    }
+    getEl(selector){
+        return document.querySelector(selector);
+    }
+    // 初始化函数
+    init(){
+        this.getStream();
+        this.createSocket('ws://192.168.22.136:8088');
 
-const video = document.querySelector('#move');
-const startBtn = document.querySelector('.start');
-const pauseBtn = document.querySelector('.pause');
-const stopBtn = document.querySelector('.stop');
-
-// 连接远程服务器
-var client = new WebSocket('ws://192.168.30.1:8088');
-
-	// 当客户端连接成功之后就会触发open事件
-client.onopen = ()=>{
-    console.log("客户端连接成功");
-}
-
-client.onmessage = function(ev){
-  console.log('my:', ev);
-}
-
-desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
-  for (const source of sources) {
-    if (source.name === "Entire Screen") {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          // {
-          // 	mandatory: {
-          // 		chromeMediaSource: 'desktop'
-          // 	}
-          // },
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: source.id,
-              maxWidth: window.screen.width,
-		      maxHeight: window.screen.height,
-              minHeight: 500,
-              maxHeight: 500
+    }
+    // 创建socket连接
+    createSocket(url){
+        this.client = new WebSocket(url);
+        this.client.addEventListener('open', (ev)=> this.handleOpen(ev));
+        this.client.addEventListener('close', (ev)=> this.handleClose(ev));
+        this.client.addEventListener('error', (ev)=> this.handleError(ev));
+        this.client.addEventListener('message', (ev)=> this.handleEessage(ev));
+    }
+    // 客户端连接服务器成功
+    handleOpen(ev){
+        console.log('客户端连接成功');
+    }
+    // 客户端断开服务器连接
+    handleClose(ev){
+        console.log("客户端断开连接");
+    }
+    // 
+    handleError(ev){
+        console.log("客户端连接出错");
+    }
+    // 
+    handleEessage(ev){
+        // console.log("获取服务端推送的消息")
+    }
+    // 获取录制视频
+    getStream(){
+        desktopCapturer.getSources({
+            types: ['window', 'screen']
+        }).then(async sources => {
+            for (const source of sources) {
+                if (source.name === "Entire Screen") {
+                    try {
+                        const stream = await navigator.mediaDevices.getUserMedia({
+                            audio: false,
+                                video: {
+                                mandatory: {
+                                    chromeMediaSource: 'desktop',
+                                    chromeMediaSourceId: source.id,
+                                    maxWidth: window.screen.width,
+                                    maxHeight: window.screen.height,
+                                    minHeight: 500,
+                                    maxHeight: 500
+                                }
+                            }
+                        })
+                        this.handleStream(stream)
+                    } catch (e) {
+                        console.log(e);
+                    }
+                    return
+                }
             }
-          }
         })
-        handleStream(stream)
-      } catch (e) {
-        handleError(e)
-      }
-      return
     }
-  }
-})
-
-function handleStream (stream) {
-  video.srcObject = stream
-  video.onloadedmetadata = (e) => video.play();
-  createRecorder(stream);
-}
-
-function handleError (e) {
-  console.log(e)
-}
-
-
-
-function createRecorder(stream){
-	let recorder = new MediaRecorder(stream, {
-		mimeType: 'video/webm;codecs=vp8'
-	});
-	startBtn.onclick = ()=>{
-		recorder.start(1000);
-  }
-  pauseBtn.onclick = ()=>{
-    
-    if(recorder.state === 'paused'){
-      recorder.resume();
-    } 
-    else if(recorder.state === 'recording'){
-      recorder.pause();
+    // 处理视频流
+    handleStream(stream){
+        this.video.srcObject = stream
+        this.video.onloadedmetadata = (e) => this.video.play();
+        this.createRecorder(stream);
     }
-    console.log(recorder)
-  }
-	stopBtn.onclick = ()=>{
-		recorder.stop();
-		// playVideo();
-	}
-	recorder.ondataavailable = event =>{
-    client.send(event.data);
-	}
-	recorder.onerror = e =>{
-		console.log(e);
-  }
-  recorder.onresume = e=>{
-    console.log("恢复录制了");
-  }
-  recorder.onpause = e=>{
-    console.log("暂停录制")
-  }
-  recorder.onstart = e=>{
-    console.log("开始录制")
-  }
+    // 录制视频流
+    createRecorder(stream){
+        let recorder = new MediaRecorder(stream, {
+            mimeType: 'video/webm;codecs=vp8'
+        });
+        // 开始录制
+        this.getEl('.start').onclick = ()=>{
+            recorder.start(1000);
+        }
+
+        // 暂停/继续播放
+        this.getEl('.pause').onclick = ()=>{
+            if(recorder.state === 'paused'){
+                recorder.resume();
+            } 
+            else if(recorder.state === 'recording'){
+                recorder.pause();
+            }
+        }
+        this.getEl('.stop').onclick = ()=>{
+            recorder.stop();
+        }
+        recorder.ondataavailable = event =>{
+            this.client.send(event.data);
+        }
+        recorder.onerror = e =>{
+            console.log(e);
+        }
+        recorder.onresume = e=>{
+            console.log("恢复录制了");
+        }
+        recorder.onpause = e=>{
+            console.log("暂停录制")
+        }
+        recorder.onstart = e=>{
+            console.log("开始录制")
+        }
+    }
 }
 
-
-
-async function sourceopen(e){
-	URL.revokeObjectURL(video2.src);
-    var mime = 'video/webm;codecs=vp8';
-    sourceBuffer = mediaSource.addSourceBuffer(mime);
-	for(let i = 0; i<chunks.length; i++){
-		await new Promise((resolve, reject)=>{
-	    	setTimeout(()=>{
-	    		if(i == 1 || i == 2){
-	    			return resolve();
-	    		}
-		    	chunks[i].arrayBuffer().then(res=>{
-		    	sourceBuffer.appendBuffer(res);
-		    		console.log('第'+i+ '段')
-		    		resolve();
-		    	})
-	    	}, 500)
-		})
-	}
-}
+// 实例化对象
+new Initiate().init();
